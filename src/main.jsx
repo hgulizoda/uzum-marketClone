a01ac4cb076a1c8b6f607678018221e974a14af2
hgulizoda <hgulizodait@gmail.com>
2025-11-09 20:54:38 +0500

    main jsx arranged

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
-import "./i18n.js";
 import "./index.css";
 import App from "./App.jsx";
+import "./i18n.js";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
