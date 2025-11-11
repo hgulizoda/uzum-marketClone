1852e5cb0ad31c7b42fb5429fcb5f7f0d7aefeb1
hgulizoda <hgulizodait@gmail.com>
2025-11-11 09:34:06 +0500

    some styling and translation done on cart

--- src/components/cartCard.jsx
+++ src/components/cartCard.jsx
@@ -12,39 +12,80 @@ import {
 
 import DeleteIcon from "@mui/icons-material/Delete";
 import uzumCard from "../assets/icons/uzumCard.svg";
+import { useTranslation } from "react-i18next";
 const CartCard = (product) => {
+  const { t } = useTranslation();
   return (
     <Box sx={{ paddingBlock: "20px", borderBottom: "1px solid lightgray" }}>
-      <Stack>
-        <Typography sx={{ fontSize: "12px", color: "gray" }}>
-          Uzum market yetkazib berishi
-        </Typography>
-        <Typography
-          variant="h3"
-          sx={{ fontSize: "18px", fontWeight: "700", marginBottom: "12px" }}
-        >
-          Ertaga yetkazib beramiz
-        </Typography>
-      </Stack>
+      <Typography sx={{ fontSize: "12px", color: "gray" }}>
+        {t("cart.deliveredByUzumMarket")}
+      </Typography>
+      <Typography
+        variant="h3"
+        sx={{ fontSize: "18px", fontWeight: "700", marginBottom: "12px" }}
+      >
+        {t("cart.tomorrowDelivery")}
+      </Typography>
+
       <Stack direction="row" justifyContent="space-between">
-        <FormControlLabel control={<Checkbox defaultChecked />} label="" />
-        <CardMedia
-          component="img"
-          image={product.thumbnail}
-          height="120px"
-          sx={{ width: "100px", backgroundColor: "secondary.main" }}
-        />
-        <Stack width="60%">
-          <Typography variant="paragraph">{product.title}</Typography>
-          <Stack direction="row" justifyContent="space-between">
-            <Typography sx={{ fontSize: "14px", marginTop: "10px" }}>
-              <span style={{ color: "gray" }}>Sotuvchi: </span> {product.brand}
-            </Typography>
-            <ButtonGroup>
-              <IconButton sx={{ borderRadius: "10%" }}>+</IconButton>
-              <IconButton>{product.count}</IconButton>
-              <IconButton>-</IconButton>
-            </ButtonGroup>
+        <Stack direction="row" spacing="20px">
+          <FormControlLabel control={<Checkbox defaultChecked />} label="" />
+          <CardMedia
+            component="img"
+            image={product.thumbnail}
+            height="120px"
+            sx={{ width: "100px", backgroundColor: "secondary.main" }}
+          />
+          <Stack>
+            <Typography variant="paragraph">{product.title}</Typography>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              spacing="150px"
+              marginTop="10px"
+            >
+              <Typography sx={{ fontSize: "14px" }}>
+                <span style={{ color: "gray" }}>{t("cart.seller")}: </span>{" "}
+                {product.brand}
+              </Typography>
+              <ButtonGroup
+                variant="outlined"
+                sx={{ border: "1px solid lightgray" }}
+              >
+                <IconButton
+                  sx={{
+                    borderRadius: "10%",
+                    backgroundColor: "secondary.main",
+                    width: "40px",
+                    height: "40px",
+                  }}
+                >
+                  +
+                </IconButton>
+                <div
+                  style={{
+                    width: "40px",
+                    height: "40px",
+                    fontSize: "18px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  {product.count}
+                </div>
+                <IconButton
+                  sx={{
+                    borderRadius: "10%",
+                    backgroundColor: "secondary.main",
+                    width: "40px",
+                    height: "40px",
+                  }}
+                >
+                  -
+                </IconButton>
+              </ButtonGroup>
+            </Stack>
           </Stack>
         </Stack>
 
@@ -60,7 +101,7 @@ const CartCard = (product) => {
               marginBottom: "20px",
             }}
           >
-            Yo'q qilish
+            {t("cart.remove")}
           </Button>
           <Typography color="primary.main" fontWeight="700" fontSize="20px">
             $
@@ -71,7 +112,9 @@ const CartCard = (product) => {
             <img src={uzumCard} alt="" style={{ marginLeft: "10px" }} />
           </Typography>
 
-          <Typography>Uzum kartasiz ${product.price}</Typography>
+          <Typography fontWeight="bold">
+            {t("cart.withoutCard")}: ${product.price}
+          </Typography>
         </Stack>
       </Stack>
     </Box>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -10,7 +10,7 @@ import useAppContext from "../hooks/useAppContext";
 import CartCard from "../components/cartCard";
 import DeliveryDiningIcon from "@mui/icons-material/DeliveryDining";
 import { useTranslation } from "react-i18next";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 
 const Cart = () => {
   const { cart } = useAppContext();
@@ -35,164 +35,206 @@ const Cart = () => {
 
   return (
     <main className="container">
-      <h2 style={{ marginBlock: "30px 20px" }}>
-        {t("cart.yourCart")},
-        <span style={{ color: "gray" }}>
-          {cart.length} {t("cart.products")}
-        </span>
-      </h2>
-      <Stack width="100%" direction="row" justifyContent="space-between">
-        <Box
-          sx={{
-            border: "1px solid lightgray",
-            borderRadius: "8px",
-            padding: "16px 16px 0",
-            width: "68%",
-          }}
-        >
+      {cart.length ? (
+        <>
+          <h2 style={{ marginBlock: "30px 20px", display: "flex", gap: "3px" }}>
+            {t("cart.yourcart")},
+            <span style={{ color: "gray" }}>
+              {cart.length} {t("cart.products")}
+            </span>
+          </h2>
           <Stack
-            className="input-group"
+            width="100%"
             direction="row"
-            spacing="10px"
-            paddingBottom="  16px"
-            sx={{ borderBottom: " 1px solid lightgray" }}
-          >
-            <FormControlLabel
-              control={<Checkbox defaultChecked />}
-              label={t("cart.selectAll")}
-            />
-          </Stack>
-
-          <Stack>
-            {cart.length ? (
-              cart.map((p) => <CartCard key={p.id} {...p} />)
-            ) : (
-              <p>Mahsulot topilmadi</p>
-            )}
-          </Stack>
-        </Box>
-
-        <Box
-          sx={{
-            border: "1px solid lightgray",
-            borderRadius: "8px",
-            padding: "16px 16px 0",
-            width: "31%",
-          }}
-        >
-          <Typography
-            variant="h6"
-            sx={{ textAlign: "center", fontWeight: "bold", fontSize: "16px" }}
-          >
-            {t("cart.deliveringToPickUps")} $5
-          </Typography>
-          <Typography
-            variant="h6"
-            sx={{ textAlign: "center", fontSize: "14px", color: "gray" }}
-          >
-            {/* {t("header.moreInfo", { neededAmount, deliveryFee })} */}
-          </Typography>
-          <Box
-            sx={{
-              height: "5px",
-              borderRadius: "11%",
-              width: "100%",
-              backgroundColor: "secondary.main",
-              marginTop: "10px",
-            }}
+            justifyContent="space-between"
+            alignItems="start"
           >
             <Box
               sx={{
-                height: "100%",
-                borderRadius: "5px",
-                width:
-                  totalPriceWithUzumCard < 50
-                    ? `${(33 * totalPriceWithUzumCard) / 50}%`
-                    : totalPriceWithUzumCard < 100 &&
-                      totalPriceWithUzumCard > 50
-                    ? (66 * totalPriceWithUzumCard) / 100
-                    : totalPriceWithUzumCard < 150 &&
-                      totalPriceWithUzumCard > 100
-                    ? (100 * totalPriceWithUzumCard) / 150
-                    : "100%",
-                backgroundColor: "primary.main",
+                border: "1px solid lightgray",
+                borderRadius: "8px",
+                padding: "16px 16px 0",
+                width: "68%",
               }}
-            ></Box>
-          </Box>
+            >
+              <Stack
+                className="input-group"
+                direction="row"
+                spacing="10px"
+                paddingBottom="  16px"
+                sx={{ borderBottom: " 1px solid lightgray" }}
+              >
+                <FormControlLabel
+                  control={<Checkbox defaultChecked />}
+                  label={t("cart.selectAll")}
+                />
+              </Stack>
 
-          <Stack
-            direction="row"
-            justifyContent="space-between"
-            marginBlock="5px"
-            paddingBottom="10px"
-            sx={{ borderBottom: "1px solid lightgray" }}
-          >
-            <Typography sx={{ fontSize: "11px", color: "gray" }}>$7</Typography>
-            <Typography sx={{ fontSize: "11px", color: "gray" }}>$5</Typography>
-            <Typography sx={{ fontSize: "11px", color: "gray" }}>$3</Typography>
-            <Typography sx={{ fontSize: "11px", color: "gray" }}>$0</Typography>
-          </Stack>
-
-          <Box>
-            <Typography fontSize="16px" marginBlock="18px" fontWeight="550">
-              Buyurtmangiz
-            </Typography>
-            <Stack direction="row" justifyContent="space-between">
-              <Typography fontSize="14px">
-                Mahsulotlar ({cart.length}):
-              </Typography>
-              <Typography fontSize="14px">${totalPrice.toFixed(2)}</Typography>
-            </Stack>
-
-            <Typography fontSize="16px" marginBlock="12px" fontWeight="550">
-              Jami
-            </Typography>
-
-            <Stack direction="row" justifyContent="space-between">
-              <Typography fontSize="14px">Uzum karta bilan</Typography>
               <Stack>
-                <Typography
+                {cart.length ? (
+                  cart.map((p) => <CartCard key={p.id} {...p} />)
+                ) : (
+                  <p>Mahsulot topilmadi</p>
+                )}
+              </Stack>
+            </Box>
+
+            <Box
+              sx={{
+                border: "1px solid lightgray",
+                borderRadius: "8px",
+                padding: "16px 16px 0",
+                width: "31%",
+              }}
+            >
+              <Typography
+                variant="h6"
+                sx={{
+                  textAlign: "center",
+                  fontWeight: "bold",
+                  fontSize: "16px",
+                }}
+              >
+                {t("cart.deliveringToPickUps")} $5
+              </Typography>
+
+              <Box
+                sx={{
+                  height: "5px",
+                  borderRadius: "11%",
+                  width: "100%",
+                  backgroundColor: "secondary.main",
+                  marginTop: "10px",
+                }}
+              >
+                <Box
                   sx={{
-                    textAlign: "right",
-                    color: "primary.main",
-                    fontSize: "20px",
-                    fontWeight: "bold",
+                    height: "100%",
+                    borderRadius: "5px",
+                    width: `${
+                      totalPriceWithUzumCard < 150
+                        ? (totalPriceWithUzumCard * 100) / 150
+                        : 100
+                    }%`,
+
+                    backgroundColor: "primary.main",
                   }}
-                >
-                  ${totalPriceWithUzumCard.toFixed(2)}
+                ></Box>
+              </Box>
+
+              <Stack
+                direction="row"
+                justifyContent="space-between"
+                marginBlock="5px"
+                paddingBottom="10px"
+                sx={{ borderBottom: "1px solid lightgray" }}
+              >
+                <Typography sx={{ fontSize: "11px", color: "gray" }}>
+                  $7
                 </Typography>
-                <Typography color="#0ebb48ff" fontSize="14px" fontWeight="bold">
-                  Tejovingiz: $
-                  {(totalPrice - totalPriceWithUzumCard).toFixed(2)}
+                <Typography sx={{ fontSize: "11px", color: "gray" }}>
+                  $5
+                </Typography>
+                <Typography sx={{ fontSize: "11px", color: "gray" }}>
+                  $3
+                </Typography>
+                <Typography sx={{ fontSize: "11px", color: "gray" }}>
+                  $0
                 </Typography>
               </Stack>
-            </Stack>
-            <Stack
-              direction="row"
-              justifyContent="space-between"
-              sx={{ marginTop: "20px" }}
-            >
-              <Typography fontSize="14px">Uzum kartasiz</Typography>
-              <Typography fontWeight="bold">
-                ${totalPrice.toFixed(2)}
-              </Typography>
-            </Stack>
-          </Box>
-          <Button
-            variant="contained"
-            sx={{
-              padding: "10px 30px",
-              width: "100%",
-              fontSize: "16px",
-              borderRadius: "10px",
-              fontWeight: "600",
-              marginTop: "20px",
-            }}
-          >
-            Rasmiylashtirishga o'tish
-          </Button>
+
+              <Box>
+                <Typography fontSize="16px" marginBlock="18px" fontWeight="550">
+                  {t("cart.yourOrder")}
+                </Typography>
+                <Stack direction="row" justifyContent="space-between">
+                  <Typography fontSize="14px">
+                    {t("cart.products")} ({cart.length}):
+                  </Typography>
+                  <Typography fontSize="14px">
+                    ${totalPrice.toFixed(2)}
+                  </Typography>
+                </Stack>
+
+                <Typography fontSize="16px" marginBlock="12px" fontWeight="550">
+                  {t("cart.total")}
+                </Typography>
+
+                <Stack direction="row" justifyContent="space-between">
+                  <Typography fontSize="14px">{t("cart.widthCard")}</Typography>
+                  <Stack>
+                    <Typography
+                      sx={{
+                        textAlign: "right",
+                        color: "primary.main",
+                        fontSize: "20px",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      ${totalPriceWithUzumCard.toFixed(2)}
+                    </Typography>
+                    <Typography
+                      color="#0ebb48ff"
+                      fontSize="14px"
+                      fontWeight="bold"
+                    >
+                      {t("cart.savings")}: $
+                      {(totalPrice - totalPriceWithUzumCard).toFixed(2)}
+                    </Typography>
+                  </Stack>
+                </Stack>
+                <Stack
+                  direction="row"
+                  justifyContent="space-between"
+                  sx={{ marginTop: "20px" }}
+                >
+                  <Typography fontSize="14px">
+                    {t("cart.withoutCard")}
+                  </Typography>
+                  <Typography fontWeight="bold">
+                    ${totalPrice.toFixed(2)}
+                  </Typography>
+                </Stack>
+              </Box>
+              <Button
+                variant="contained"
+                sx={{
+                  padding: "10px 30px",
+                  width: "100%",
+                  fontSize: "16px",
+                  borderRadius: "10px",
+                  fontWeight: "600",
+                  marginTop: "20px",
+                }}
+              >
+                {t("cart.checkout")}
+              </Button>
+            </Box>
+          </Stack>
+        </>
+      ) : (
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "center",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "150px",
+          }}
+        >
+          <img
+            src="https://uzum.uz/static/img/shopocat.490a4a1.png"
+            alt=""
+            style={{ width: "150px" }}
+          />
+          <p style={{ fontWeight: "bold" }}>{t("cart.notFound")}</p>
+          <p style={{ fontSize: "12px", color: "gray", marginTop: "10px" }}>
+            {t("cart.startFromHome")}
+          </p>
         </Box>
-      </Stack>
+      )}
     </main>
   );
 };
